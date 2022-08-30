a5b733dd0e7a76097b092a443a4d2b9f3c96f4d1
spences10 <spences10apps@gmail.com>
2022-08-30 20:33:19 +0100

    refactor: :recycle: props from page endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	let user: string = $page.url.searchParams.get('user')?.toString() || '';
 
 	export let data: any;
+
+	// const getData = async () => {
+	// 	const res = await fetch(`${$page.url}github-contributions.json?user=spences10`);
+	// 	if (res.ok) {
+	// 		const data = await res.json();
+	// 		return {
+	// 			data
+	// 		};
+	// 	}
+	// };
+
+	// let userData: { data: any } | undefined;
+	// onMount(async () => {
+	// 	userData = await getData();
+	// 	console.log(userData);
+	// });
 </script>
 
-<pre>{JSON.stringify(user, null, 2)}</pre>
 <pre>{JSON.stringify(data, null, 2)}</pre>
 
 <h1>Welcome to SvelteKit</h1>

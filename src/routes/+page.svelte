6ceb671c47ead75db06fc0c98b68abbde3ca7de1
spences10 <spences10apps@gmail.com>
2023-05-13 19:51:46 +0100

    refactor: :recycle: switch over to new template
    use tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
-
-	let user: string = $page.url.searchParams.get('user')?.toString() || '';
-
-	export let data: any;
-</script>
-
-<pre>{JSON.stringify(data, null, 2)}</pre>
-
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	documentation
+</p>

64849fdef84c3ac58c11ca03c91e660263d6a7d6
spences10 <spences10apps@gmail.com>
2024-09-15 21:02:25 +0100

    refactor: :recycle: svelte 5 children

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
 	import '../app.css';
+
+	let { children } = $props();
 </script>
 
-<div
-	class="flex min-h-screen flex-col items-center justify-center bg-base-200 p-4"
->
-	<main
-		class="container mx-auto max-w-3xl rounded-box bg-base-100 p-8 shadow-xl"
-	>
-		<h1 class="mb-8 text-center text-3xl font-bold">
-			GitHub Contribution Tracker
-		</h1>
-		<slot />
+<div class="flex min-h-screen flex-col bg-base-200">
+	<main class="flex flex-grow items-center justify-center p-4">
+		<div
+			class="container mx-auto max-w-3xl rounded-box bg-base-100 p-8 shadow-xl"
+		>
+			<h1 class="mb-8 text-center text-3xl font-bold">
+				GitHub Contribution Tracker
+			</h1>
+			{@render children()}
+		</div>
 	</main>
-</div>
 
-<footer
-	class="footer footer-center bg-base-300 p-4 text-base-content"
->
-	<div>
-		<p>
-			Copyright &copy; 2017 - {new Date().getFullYear()} - All rights reserved
-			by Scott Spence
-		</p>
-	</div>
-</footer>
+	<footer
+		class="footer footer-center bg-base-300 p-4 text-base-content"
+	>
+		<div>
+			<p>
+				Copyright &copy; 2017 - {new Date().getFullYear()} - All rights
+				reserved by Scott Spence
+			</p>
+		</div>
+	</footer>
+</div>
